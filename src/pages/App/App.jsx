666bf1acf3a27c867a4a81ca7e3d7c30fa12266e
brycerobinsn <bryce.robinson@gmail.com>
2022-12-06 22:11:20 -0800

    add basic navbar

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,29 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom'
+import AuthPage from '../AuthPage/AuthPage';
+import DrinkListPage from '../DrinkListPage/DrinkListPage';
+import FavoritePage from '../FavoritePage/FavoritePage';
+import NavBar from '../../components/NavBar/NavBar';
+import IngListPage from '../IngListPage/IngListPage';
 
 function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({})
 
   return (
     <main>
-      App
+      {user ?
+        <>
+        <NavBar/>
+          <Routes>
+            <Route path='/drinks' element={<DrinkListPage/>}/>
+            <Route path='/drinks/favorite' element={<FavoritePage/>}/>
+            <Route path='/ingredients' element={<IngListPage/>}/>
+          </Routes>
+        </>
+          :
+          <AuthPage/>
+      }
     </main>
   );
 }
