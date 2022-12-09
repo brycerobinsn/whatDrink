77049111661d08a3d142e64c8893257a68fd2727
brycerobinsn <bryce.robinson@gmail.com>
2022-12-08 21:37:48 -0800

    add drinkListForm to DrinkListPage

---
+++ src/components/DrinkListForm/DrinkListForm.jsx
@@ -0,0 +1,20 @@
+
+
+export default function DrinkListForm (){
+
+    return(
+        <div className="form-container">
+            <form>
+                <label>Alcohol</label>
+                <select>
+                    <option value='gin'>Gin</option>
+                    <option value='vodka'>Vodka</option>
+                    <option value='tequila'>Tequila</option>
+                    <option value='whiskey'>Whiskey</option>
+                    <option value='rum'>Rum</option>
+                </select>
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    )
+}
